d4f11c185c0e5d9bf83c6f3709212e9fc0d8e697
QuentinDesableau <quentin.desableau@gmail.com>
2018-03-26 16:13:34 +0200

    Add files via upload

---
+++ DemoTest/MathHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoTest
+{
+    public static class MathHelper
+    {
+        public static long Factorielle (int a)
+        {
+            if (a <= 1)
+            {
+                return 1;
+            }
+            return a * Factorielle (a - 1);
+        }
+    }
+}
---
+++ DemoTest/Meteo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoTest
+{
+    public class Meteo
+    {
+        public double Temperature { get; set; }
+
+        public Temps Temps { get; set; }
+
+    }
+}
